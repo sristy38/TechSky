6be9d58b1db5c45af613330dc1f898e3501c40f3
sristy38 <107880699+sristy38@users.noreply.github.com>
2022-09-08 12:04:08 +0600

    knowmore

---
+++ knowmore.php
@@ -0,0 +1,234 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link href="https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>KNOW MORE</title>
+</head>
+
+<body data-bs-spy="scroll" data-bs-target=".navbar" data-bs-offset="70">
+
+
+    <!-- TOP NAV -->
+    <div class="top-nav" id="home">
+        <div class="container">
+            <div class="row justify-content-between">
+                <div class="col-auto">
+                    <p> <i class="bx bxs-envelope"></i> email@gmail.com</p>
+                    <p> <i class="bx bxs-phone-call"></i> (219) 555-0114</p>
+                </div>
+                <div class="col-auto social-icons">
+                    <a href="#"><i class="bx bxl-facebook"></i></a>
+                    <a href="#"><i class="bx bxl-twitter"></i></a>
+                    <a href="#"><i class="bx bxl-instagram"></i></a>
+                    <a href="#"><i class="bx bxl-pinterest"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- BOTTOM NAV -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-white sticky-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">TechSky<span class="dot">.</span></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="aboutus.php">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="services.html">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#portfolio">Portfolio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="team.php">Team</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#blog">Blog</a>
+                    </li>
+                </ul>
+                <a href="#" data-bs-toggle="modal" data-bs-target="#exampleModal" class="btn btn-brand ms-lg-3">Contact</a>
+                <a href="#" data-bs-toggle="modal" data-bs-target="#exampleModal" class="btn btn-brand ms-lg-3">login</a>
+            </div>
+        </div>
+    </nav>
+
+
+
+    <div class="know-image w-250 vh-100 d-flex justify-content-center align-items-center">
+        <div class="content text-center">
+            <h1 class="text-white">
+                <h3 class="teambgtext">APPLICATION<br><b>DEVELOPMENT Services</b></h3>
+        </div>
+    </div>
+    <section class="knowsection">
+        <div class="team_t1">
+            <h5 align="center">PROFESSIONALS WITH ABSTRACTION LAYERS</h5>
+            <p align="center"> We have a great working environment with a rich culture of fun and professionalism that
+                attracts the best talents. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus,
+                quisquam facilis consectetur cumque nobis totam porro a distinctio aspernatur? Ratione numquam tempore
+                optio rerum accusamu</p>
+        </div>
+    </section>
+
+
+    <section class="pgrid">
+        <div class="grid-col-2">
+
+            <div class="team_t1">
+                <h5 align="center">GLOBALLY DIVERSE PERSPECTIVE</h5>
+            </div>
+
+            <div class="team_t1">
+
+                <p align="justify"> We have developed products that have impacted people and businesses on different
+                    continents while maintaining a local connection. Our expansive experience working with new age
+                    technologies, for diverse audiences over the past decade helps us to craft a fit-for-purpose
+                    solution regardless of your target market.</p>
+            </div>
+
+
+        </div>
+
+
+        <div class="grid-col-2">
+
+            <div class="team_t1">
+                <h5 align="center">SOFTWARE DEVELOPMENT,DE-RISKED</h5>
+            </div>
+
+            <div class="team_t1">
+
+                <p align="justify"> A new paradigm in software development that empowers the client and emboldens the
+                    blueprint for success. With our milestone-based engagement plan, you only pay for one milestone at a
+                    time, limiting your investment exposure and keeping us honest. Code-reviewed deliverables guarantee
+                    that you are not sold lemons but rather a top-shelf output by a trusted partner
+            </div>
+
+
+        </div>
+
+        <div class="grid-col-2">
+
+            <div class="team_t1">
+                <h5 align="center">VISIBILITY AND CONTROL</h5>
+            </div>
+
+            <div class="team_t1">
+
+                <p align="justify"> Never feel lost or in the dark about your product. With proprietary tools and
+                    processes, take full control of the delivery process and see the progress of your product,
+                    on-demand. Monitor project output through our Project Pulse tool and experience better involvement
+                    in your product’s lifecycle.
+            </div>
+
+
+        </div>
+
+        <div class="grid-col-2">
+
+            <div class="team_t1">
+                <h5 align="center">OUR TALENT COLONY</h5>
+            </div>
+
+            <div class="team_t1">
+
+                <p align="justify"> A cast of highly motivated Engineers, Data Scientists, Designers, Strategists and
+                    Thinkers with a common flair for innovation and excellence. TechSky vets talent using one of the
+                    most stringent assessment methodologies in the industry to ensure that our promise of exceeding
+                    satisfaction is kept.
+            </div>
+
+
+        </div>
+
+
+    </section>
+
+    <section class="knowsection">
+        <div class="team_t1">
+            <h5 align="center">Want to Know More About Application Development Service?</h5>
+
+            <p align="center"><button type="button" class="btn btn-primary btnService">Request info</button></p>
+        </div>
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- FOOTER -->
+    <footer>
+        <div class="footer-top text-center footerDesign">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-6 text-center">
+                        <h4 class="navbar-brand">TechSky<span class="dot">.</span></h4>
+                        <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of
+                            classical Latin literature from</p>
+                        <div class="col-auto social-icons">
+                            <a href="#"><i class='bx bxl-facebook'></i></a>
+                            <a href="#"><i class='bx bxl-twitter'></i></a>
+                            <a href="#"><i class='bx bxl-instagram'></i></a>
+                            <a href="#"><i class='bx bxl-pinterest'></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-bottom text-center">
+            <p class="mb-0">Copyright@2020. All rights Reserved</p>
+        </div>
+    </footer>
+
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/app.js"></script>
+
+</body>
+
+</html>
